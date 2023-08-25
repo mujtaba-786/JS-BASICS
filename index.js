c710e97a06d593b341957677251231b4e8de4060
mujtaba-786 <mohdmujtaba11042000>
2023-08-24 21:44:53 -0400

    expense tracker

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
-var submitt = document.getElementById("submit")
-var dele  = document.getElementById("del")
-var editt = document.getElementById("edit")
+var expense = document.getElementById("exp")
+var description = document.getElementById("descr")
+var category = document.getElementById("categ")
+var tabl = document.createElement("table")
+var c2 = document.getElementById("container2")
+var count=0
 
+btn.addEventListener("click" , (e)=>{
+    
+    var row=""
+    row+=count
+    //console.log(row)
+    var row = tabl.insertRow(0);
+    //console.log(row[row.])
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
 
-submitt.addEventListener("click", (e) =>{
+    cell1.innerHTML = expense.value,
+    cell2.innerHTML = description.value,
+    cell3.innerHTML = category.value
+    cell4.innerHTML = "<button>Delete</button>"
+    cell5.innerHTML = "<button>Edit</button>"
+    c2.appendChild(tabl)
     
-    var obj = {
-        name : document.getElementById("name").value,
-        email : document.getElementById("email").value,
-        phone : document.getElementById("phone").value
-    }
-    
-    
-    
-    var p=obj.email
-    var obj = JSON.stringify(obj)
-    localStorage.setItem(p,obj)
-    dele.addEventListener("click", (e) =>{
-        localStorage.removeItem(p)
-        console.log(p)
-        document.getElementById("name").value=""
-        document.getElementById("email").value=""
-        document.getElementById("phone").value=""
+    cell4.addEventListener("click" ,(e)=>{ 
+        row.deleteCell(0)
+        row.deleteCell(0)
+        row.deleteCell(0)
+        row.deleteCell(0)
+        row.deleteCell(0)
     })
-    
-    
+
+    // cell5.addEventListener("click" , ()=>{
+    //      //document.getElementById("exp").value = this.cells[0].innerHTML
+    //      tabl.rows[0].cells[0].innerHTML = 12
+    // })
+
+    count++
 })
-
-editt.addEventListener("click", ()=>{
-    document.getElementById("email").value=""
-})
-
-
-
